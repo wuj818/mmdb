f583dc2d0febfd49df4bb98c94e629d5ba70c837
James Wu <wuj818@gmail.com>
2010-10-03 23:02:54 -0400

    adding validations for movies and more ui tweaks

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ActiveRecord::Base
+  validates :title,
+    :presence => true
+
+  validates :imdb_url,
+    :presence => true,
+    :uniqueness => true
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,4 +1,23 @@
 require 'spec_helper'
 
 describe Movie do
+  describe "validations" do
+    before(:all) do
+      @movie = Movie.new
+      @movie.save
+    end
+
+    it "should have required attributes" do
+      [:title, :imdb_url].each do |attribute|
+        @movie.errors[attribute].should include "can't be blank"
+      end
+    end
+
+    it "should have a unique IMDB url" do
+      movie = Movie.make!
+      @movie.imdb_url = movie.imdb_url
+      @movie.save
+      @movie.errors[:imdb_url].should include 'has already been taken'
+    end
+  end
 end
