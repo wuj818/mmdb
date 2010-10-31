72efb8ee8d9219eaabc951f4a1a55d1300c01ac1
James Wu <wuj818@gmail.com>
2010-10-30 20:05:32 -0400

    showing ajax indicator on movie edit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,7 +5,7 @@ $(function() {
     $(this).prev().attr('checked', !$(this).prev().is(':checked'));
   });
 
-  $('#new_from_imdb').live('click', function() {
-    $('#new_from_imdb_ajax').fadeIn();
+  $('#ajax_button').live('click', function() {
+    $('#ajax_button_indicator').fadeIn();
   });
 });
