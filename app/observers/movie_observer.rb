0b8dacb92c4d4d971cc28e60c4fd7fe21746bb5f
James Wu <wuj818@gmail.com>
2010-11-27 11:55:53 -0500

    using instance variable for diggler in movie observer

--- app/observers/movie_observer.rb
+++ app/observers/movie_observer.rb
@@ -1,6 +1,7 @@
 class MovieObserver < ActiveRecord::Observer
   def after_create(movie)
     return if Rails.env.test?
+    @diggler = DirkDiggler.new movie.imdb_url
     get_tags(movie)
     get_people(movie)
   end
@@ -8,51 +9,49 @@ class MovieObserver < ActiveRecord::Observer
   private
 
   def get_tags(movie)
-    diggler = DirkDiggler.new movie.imdb_url
-    diggler.get :genres, :keywords, :languages, :countries
+    @diggler.get :genres, :keywords, :languages, :countries
 
-    movie.genre_list = diggler.genres
-    movie.keyword_list = diggler.keywords
-    movie.language_list = diggler.languages
-    movie.country_list = diggler.countries
+    movie.genre_list = @diggler.genres
+    movie.keyword_list = @diggler.keywords
+    movie.language_list = @diggler.languages
+    movie.country_list = @diggler.countries
     movie.save
   end
 
   def get_people(movie)
-    diggler = DirkDiggler.new movie.imdb_url
-    diggler.get :directors, :writers, :composers, :editors, :cinematographers, :actors
+    @diggler.get :directors, :writers, :composers, :editors, :cinematographers, :actors
 
-    diggler.directors.each do |imdb_url, info|
+    @diggler.directors.each do |imdb_url, info|
       person = Person.find_or_create_by_imdb_url imdb_url
       person.update_attribute(:name, info[:name]) if person.name.blank?
       person.directing_credits.create :movie => movie, :job => 'Director', :details => info[:details]
     end
 
-    diggler.writers.each do |imdb_url, info|
+    @diggler.writers.each do |imdb_url, info|
       person = Person.find_or_create_by_imdb_url imdb_url
       person.update_attribute(:name, info[:name]) if person.name.blank?
       person.writing_credits.create :movie => movie, :job => 'Writer', :details => info[:details]
     end
 
-    diggler.composers.each do |imdb_url, info|
+    @diggler.composers.each do |imdb_url, info|
       person = Person.find_or_create_by_imdb_url imdb_url
       person.update_attribute(:name, info[:name]) if person.name.blank?
       person.composing_credits.create :movie => movie, :job => 'Composer', :details => info[:details]
     end
 
-    diggler.editors.each do |imdb_url, info|
+    @diggler.editors.each do |imdb_url, info|
       person = Person.find_or_create_by_imdb_url imdb_url
       person.update_attribute(:name, info[:name]) if person.name.blank?
       person.editing_credits.create :movie => movie, :job => 'Editor', :details => info[:details]
     end
 
-    diggler.cinematographers.each do |imdb_url, info|
+    @diggler.cinematographers.each do |imdb_url, info|
       person = Person.find_or_create_by_imdb_url imdb_url
       person.update_attribute(:name, info[:name]) if person.name.blank?
       person.cinematography_credits.create :movie => movie, :job => 'Cinematographer', :details => info[:details]
     end
 
-    diggler.actors.each do |imdb_url, info|
+    @diggler.actors.each do |imdb_url, info|
       person = Person.find_or_create_by_imdb_url imdb_url
       person.update_attribute(:name, info[:name]) if person.name.blank?
       person.acting_credits.create :movie => movie, :job => 'Actor', :details => info[:details]
