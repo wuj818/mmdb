8c921ab0664d500339c71e77c9313a12d3bdeebc
James Wu <wuj818@gmail.com>
2011-12-24 11:39:10 -0500

    removing unnecessary javascript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,45 +1,11 @@
-fancybox_settings = {
-  padding: 4,
-  cyclic: true,
-  overlayOpacity: 0.9,
-  overlayColor: '#000',
-  titlePosition: 'over',
-  speedIn: 0,
-  speedOut: 0,
-  changeSpeed: 0,
-  showCloseButton: false,
-  titleFormat: function(title, currentArray, currentIndex, currentOpts) {
-    var url = $('a[title="' + title + '"]').attr('data-movie-url');
-    return '<div id="fancybox-title-over">' +
-           '<a href="' + url + '">' + title + '</a>' +
-           '</div>';
-  },
-  onComplete: function(links, index) {
-    if ($('#env').hasClass('production')) {
-      var title = $('a[href="' + links[index] + '"]').attr('title');
-      _gaq.push(['_trackEvent', 'Posters', 'Fancybox', title]);
-    }
-  }
-}
-
 $(function() {
-  $('.close').live('click', function() { $(this).parent().fadeOut(500); });
+  $('.close').live('click', function() {
+    $(this).parent().fadeOut(500);
+  });
 
   $('#genre_checkboxes label').live('click', function() {
     $(this).prev().attr('checked', !$(this).prev().is(':checked'));
   });
-
-  // var timer;
-  // $('#search_form input').keyup(function(e) {
-  //   clearTimeout(timer);
-  //   if (e.keyCode >= 46 || e.keyCode == 8) {
-  //     timer = setTimeout(function() {
-  //       $('#search_form').submit();
-  //     }, 400);
-  //   }
-  // });
-
-  $('.tiny_poster_link').fancybox(fancybox_settings);
 
   $('#ajax_button').live('click', function() {
     $('#ajax_button_indicator').fadeIn();
@@ -56,15 +22,27 @@ $(function() {
     function() { $(this).animate({opacity: 1.0}, 200); }
   );
 
-  $('#ajax_activity')
-    .ajaxStart(function() {
-      $(this).show();
-      $('.content').animate({ opacity: 0.5 }, 200);
-    })
-    .ajaxStop(function() {
-      $(this).hide();
-      $('.content').animate({ opacity: 1.0 }, 200);
-
-      $('.tiny_poster_link').fancybox(fancybox_settings);
-    });
+  $('.tiny_poster_link').fancybox({
+    padding: 4,
+    cyclic: true,
+    overlayOpacity: 0.9,
+    overlayColor: '#000',
+    titlePosition: 'over',
+    speedIn: 0,
+    speedOut: 0,
+    changeSpeed: 0,
+    showCloseButton: false,
+    titleFormat: function(title, currentArray, currentIndex, currentOpts) {
+      var url = $('a[title="' + title + '"]').attr('data-movie-url');
+      return '<div id="fancybox-title-over">' +
+             '<a href="' + url + '">' + title + '</a>' +
+             '</div>';
+    },
+    onComplete: function(links, index) {
+      if ($('#env').hasClass('production')) {
+        var title = $('a[href="' + links[index] + '"]').attr('title');
+        _gaq.push(['_trackEvent', 'Posters', 'Fancybox', title]);
+      }
+    }
+  });
 });
