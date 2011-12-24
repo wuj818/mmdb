31b79d52df62f0035f03f9dda78d19fa12ba22d9
James Wu <wuj818@gmail.com>
2011-12-23 21:29:38 -0500

    removing timestamp querystring from s3 urls

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,11 @@
 Paperclip.interpolates :cdn do |attachment, style|
   "cdn#{attachment.original_filename.hash % 4}"
 end
+
+module Paperclip
+  class Attachment
+    def url(style_name = default_style, use_timestamp = @use_timestamp)
+      original_filename.nil? ? interpolate(@default_url, style_name) : interpolate(@url, style_name)
+    end
+  end
+end
