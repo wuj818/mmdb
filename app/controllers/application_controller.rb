01ea1f9c50cc06f6834c56445eed70896e028c13
James Wu <wuj818@gmail.com>
2012-02-06 23:24:33 -0500

    fixing main page css and removing periods from search queries

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,11 @@ class ApplicationController < ActionController::Base
 
   def search
     values = {}
-    values.merge!(:format => :js) if request.xhr?
     unless params[:minimum].blank?
       params[:minimum] = params[:minimum].to_i
-      values = {:minimum => params[:minimum]}
+      values = { :minimum => params[:minimum] }
     end
-    values.merge!(:q => params[:q]) unless params[:q].blank?
+    values.merge!(:q => params[:q].sub('.', '')) unless params[:q].blank?
     values.merge!(:type => params[:type]) if controller_name == 'tags'
     redirect_to send("formatted_search_#{controller_name}_path", values)
   end
