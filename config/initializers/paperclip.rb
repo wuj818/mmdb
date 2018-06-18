229e2ebb0a8905e78b3266a5fe07c919405068d8
James Wu <wuj818@gmail.com>
2018-06-18 19:11:30 -0400

    upadting cdn

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,7 +1,3 @@
-Paperclip.interpolates :cdn do |attachment, style|
-  "mmdb-cdn#{attachment.original_filename.hash % 4}"
-end
-
 module Paperclip
   class Attachment
     def url(style_name = default_style, options = {})
