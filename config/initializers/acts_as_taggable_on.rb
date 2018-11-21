acc72ee02117c0e1a2d6e17905db113b9aa85f82
James Wu <wuj818@gmail.com>
2018-11-21 02:44:25 -0500

    fixing rubocop: Layout/SpaceInsideBlockBraces

--- config/initializers/acts_as_taggable_on.rb
+++ config/initializers/acts_as_taggable_on.rb
@@ -4,7 +4,7 @@ module ActsAsTaggableOn
 
     # Fetch the values at the specified page number
     #   Model.page(5)
-    scope :page, Proc.new {|num|
+    scope :page, Proc.new { |num|
       limit(default_per_page).offset(default_per_page * ([num.to_i, 1].max - 1))
     } do
       include Kaminari::ActiveRecordRelationMethods
