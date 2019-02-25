1233fbb2ba77978b06cf2b44a319fb3ac046c08b
James Wu <wuj818@gmail.com>
2019-02-25 02:58:15 -0500

    Update production database credentials

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :forward_agent, true
 # Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
-set :shared_dirs, fetch(:shared_dirs, []).push('tmp/pids', 'tmp/sockets', 'db')
+set :shared_dirs, fetch(:shared_dirs, []).push('tmp/pids', 'tmp/sockets')
 set :shared_files, fetch(:shared_files, []).push('config/master.key')
 
 # This task is the environment that is loaded for all remote run commands, such as
@@ -60,7 +60,7 @@ task :deploy do
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
-    invoke(:'rails:db_migrate') unless ENV['migrate'].nil?
+    invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
