ab08c764bbc77d205fb5077013e301fe2bed7f60
James Wu <wuj818@gmail.com>
2019-03-15 01:09:30 -0400

    Use chruby instead of rvm in mina deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,30 +1,18 @@
 require 'mina/rails'
 require 'mina/git'
-# require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
-require 'mina/rvm' # for rvm support. (https://rvm.io)
+require 'mina/chruby'
 require 'mina/puma'
-
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
 
 set :application_name, 'mmdb'
 set :repository, 'git@github.com:wuj818/mmdb.git'
 set :branch, ENV['branch'] || 'master'
-set :rvm_path, '/usr/share/rvm/scripts/rvm'
-set :rvm_use_path, '/usr/share/rvm/scripts/rvm'
+
+set :chruby_path, '/usr/local/share/chruby/chruby.sh'
 
 set :domain, 'wuj818.com'
 set :deploy_to, '/var/www/mmdb'
 set :user, 'deploy'
 set :forward_agent, true
-
-# Optional settings:
-#   set :user, 'foobar'          # Username in the server to SSH to.
-#   set :port, '30000'           # SSH port number.
-#   set :forward_agent, true     # SSH forward_agent.
 
 # Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
@@ -35,12 +23,7 @@ set :shared_files, fetch(:shared_files, []).push('config/master.key')
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
 task :remote_environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .ruby-version or .rbenv-version to your repository.
-  # invoke :'rbenv:load'
-
-  # For those using RVM, use this to load an RVM version@gemset.
-  invoke :'rvm:use', 'default'
+  invoke :chruby, '2.6.2'
 end
 
 # Put any custom commands you need to run at setup
